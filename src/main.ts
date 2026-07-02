f71df89d82763e94d9fd6f3ff66b12f6015aff7a
pooriaashkevarvakili <pooriavakili09@gmail.com>
2026-07-02 18:16:06 +0330

    update
    update

--- src/main.ts
+++ src/main.ts
@@ -46,7 +46,7 @@ async function bootstrap() {
  app.enableCors({
   origin: [
     'http://localhost:5173',
-    'https://dashboard-project-front-kkqlto1fx-pooriavakilis-projects.vercel.app',
+    'https://dashboard-project-front-eight.vercel.app',
   ],
   credentials: true,
 });
