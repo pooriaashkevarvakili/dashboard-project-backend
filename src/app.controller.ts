84427b7c200621988dbe2c1a47abbf60d24bce8d
pooriaashkevarvakili <pooriavakili09@gmail.com>
2026-07-02 11:46:28 +0330

    api change
    api change

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { join } from 'path';
 import type { Response } from 'express';
-import data from './data/transitiom.json';
+import data from './data/transactions.json';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -15,7 +15,7 @@ export class AppController {
   getImage(@Res() res: Response) {
     return res.sendFile(join(process.cwd(), 'public', 'codekhane.jpeg'));
   }
-  @Get('news')
+  @Get('transactions')
   application(@Res() res: Response): void {
     res.status(HttpStatus.OK).json(data);
   }
