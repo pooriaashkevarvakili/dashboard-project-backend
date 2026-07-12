461679ce685b41f826f2cc539045148c89fa70c7
pooriaashkevarvakili <pooriavakili09@gmail.com>
2026-07-12 14:31:33 +0330

    fixedcapta

--- src/auth/Capta.service.ts
+++ src/auth/Capta.service.ts
@@ -4,17 +4,7 @@ import axios from 'axios';
 @Injectable()
 export class CaptchaService {
   async verify(token: string): Promise<boolean> {
-    // Hard‑code کردن کلید تست گوگل (فقط برای توسعه)
     const secretKey = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe';
-
-    // اگر خواستید هم از env و هم hard‑code استفاده کنید:
-    // const secretKey = process.env.RECAPTCHA_SECRET_KEY || '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe';
-
-    console.log('======================');
-    console.log('SECRET KEY:', secretKey);
-    console.log('TOKEN:', token);
-    console.log('======================');
-
     const { data } = await axios.post(
       'https://www.google.com/recaptcha/api/siteverify',
       null,
